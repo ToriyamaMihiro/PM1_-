12f37f065eee887530ed1c2cbd62635f44d8506c
美尋 鳥山 <k022g0003@g.neec.ac.jp>
2022-10-26 11:59:42 +0900

    third branch に変更

--- PM1_第一回/PM1_第一回.cpp
+++ PM1_第一回/PM1_第一回.cpp
@@ -7,10 +7,7 @@ int main()
 {
     std::cout << "Hello PM1!\n";
 
-    std::cout << "New branch changed\n";
-
-    std::cout << "third branch\n";
-
+    std::cout << "New branch changed \n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
